1bf1e2bad723e4eeb9187190b905661654c3c8de
young <hyangyoungs@gmail.com>
2024-06-24 11:17:27 -0500

    fix: user sign out error

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Delivery resource:
   devise_for :users
   # Routes for the Delivery resource:
-  root to: "deliveries#index"
+  root to: "users#index"
+
   # CREATE
   post("/insert_delivery", { :controller => "deliveries", :action => "create" })
-          
+
   # READ
   get("/deliveries", { :controller => "deliveries", :action => "index" })
-  
+
   get("/deliveries/:path_id", { :controller => "deliveries", :action => "show" })
-  
+
   # UPDATE
-  
+
   post("/modify_delivery/:path_id", { :controller => "deliveries", :action => "update" })
-  
+
   # DELETE
   get("/delete_delivery/:path_id", { :controller => "deliveries", :action => "destroy" })
 
